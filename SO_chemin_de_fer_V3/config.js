54a725bc0dc6e764d95f241d598056ed29e72e29
camillehispard <camillehispard@gmail.com>
2026-09-22 16:38:45 +0200

    Update config.js

--- SO_chemin_de_fer_V3/config.js
+++ SO_chemin_de_fer_V3/config.js
@@ -1,6 +1,6 @@
 // Remets ici les 2 valeurs du projet Supabase ACTUEL du Chemin de fer.
 // Project URL + Publishable key (jamais la Secret key).
 window.SO_CONFIG = {
-  url: "https://VOTRE-PROJET.supabase.co",
-  key: "sb_publishable_VOTRE_CLE"
+url: "https://gbcajaugbvvztyovpwhk.supabase.co",
+key: "TA_CLE_PUBLISHABLE"
 };
